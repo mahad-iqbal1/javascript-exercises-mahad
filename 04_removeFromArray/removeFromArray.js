838ceb377f65cdf10470260bea9fefc69f8e6b95
Mahad Iqbal <135532774+mahad-iqbal1@users.noreply.github.com>
2025-03-31 21:20:02 +0500

    Complete JS exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arg) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let toRemove = false;
+
+        for (let j = 0; j < arg.length; j++) {
+            if (array[i] === arg[j]) {
+                toRemove = true;
+                break;
+            }
+        }
+
+        if (toRemove === false) {
+            newArray.push(array[i]);
+        } 
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
