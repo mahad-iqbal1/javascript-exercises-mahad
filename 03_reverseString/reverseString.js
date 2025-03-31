1fc81432a6636b84648a5d5b88b3bbf2df2d2720
Mahad Iqbal <135532774+mahad-iqbal1@users.noreply.github.com>
2025-03-31 14:44:26 +0500

    Create a new array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringLength = string.stringLength;
+    let reversedString = [];
+    for (let i = 0; i < stringLength; i++) {
+        
+    }
 };
 
 // Do not edit below this line
