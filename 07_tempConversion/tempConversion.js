f37956ea8815e040759f3f0ad0e08af3cefcfd86
Mahad Iqbal <135532774+mahad-iqbal1@users.noreply.github.com>
2025-04-02 14:28:24 +0500

    Complete JS exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celsiusTemp = 0;
+
+  return celsiusTemp = Math.round(((farenheit - 32) * (5 / 9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let farenheitTemp = 0;
+
+  return farenheitTemp = Math.round((celsius * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
