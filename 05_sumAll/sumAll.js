6f7aa4cb356f9c93590a1d885a420f46f81a4d02
Mahad Iqbal <135532774+mahad-iqbal1@users.noreply.github.com>
2025-04-02 13:55:56 +0500

    Complete JS exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let biggerNum = 0;
+    let smallerNum = 0;
 
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    } else if (typeof(num1) !== "number" || typeof(num2) !== "number") {
+        return "ERROR"; 
+    } else {
+        if (num1 > num2) {
+            biggerNum = num1;
+            smallerNum = num2;
+        } else {
+            biggerNum = num2;
+            smallerNum = num1;
+        }
+    
+        for (let i = smallerNum; i <= biggerNum; i++) {
+            sum += i;
+        }
+    
+        return sum;
+    }
 };
 
 // Do not edit below this line
